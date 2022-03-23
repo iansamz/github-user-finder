fc6f11fc07108c2f71d44134907766679c179818
Ian Mungai <iansam74@gmail.com>
2022-03-24 02:12:34 +0300

    bio

--- src/components/GithubUser/UserBio.tsx
+++ src/components/GithubUser/UserBio.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function UserBio({ bio }: any) {
   return (
     <p className="text-sm font-medium text-gray-800 dark:text-gray-300">
-      {bio === null ? 'Not Available' :bio}
+      {bio === null ? '' :bio}
     </p>
   )
 }
