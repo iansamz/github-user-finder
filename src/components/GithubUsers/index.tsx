de00a829be81e4ee4d22dd5d1ae4c06b2a3135b7
Ian Mungai <iansam74@gmail.com>
2022-03-23 04:58:43 +0300

    github user count

--- src/components/GithubUsers/index.tsx
+++ src/components/GithubUsers/index.tsx
@@ -11,7 +11,19 @@ interface GithubUsersProps {
 }
 
 export default function GithubUsers({data}: GithubUsersProps) {
+  console.log(data)
   return (
-    <div>GithubUsers</div>
+    <div className="max-w-4xl mx-auto px-2">
+      <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+        <div className="flex justify-start lg:w-0 lg:flex-1">
+            <p className="cursor-pointer font-bold text-gray-900 dark:text-white text-lg">
+              {data.total_count} Github Users
+            </p>
+        </div>
+        <div className="flex items-center justify-end flex-1 lg:w-0">
+          
+        </div>
+      </div>
+    </div>
   )
 }
